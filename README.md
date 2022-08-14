2b5c980baf818dc6383ce24f601f33edff424ff1
984199774@qq.com <984199774@qq.com>
2022-08-15 07:10:31 +0800

    优化http请求，使用http池化技术

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # xpcloudes-opensdk
 xp-420b 条码打印机sdk。支持打印不干胶标签、各种小票订单。支持打印HTML订单，pdf文件，图文订单
+# 使用HTTP池化技术
+参考https://xww520.gitee.io/2020/03/29/HTTP%E6%B1%A0%E5%8C%96%E6%8A%80%E6%9C%AF%E7%9A%84%E8%90%BD%E5%9C%B0%E5%AE%9E%E8%B7%B5/ 这个链接，使用HTTP池化技术，提升程序执行性能
--- src/main/java/io/github/dv996coding/util/HttpClientUtil.java
+++ src/main/java/io/github/dv996coding/util/HttpClientUtil.java
@@ -1,16 +1,30 @@
 package io.github.dv996coding.util;
 
+import com.aliyuncs.utils.StringUtils;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.HttpRequest;
+import org.apache.http.NoHttpResponseException;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 
+import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLHandshakeException;
 import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.UnknownHostException;
 import java.util.Map;
 
 /**
@@ -21,9 +35,117 @@ import java.util.Map;
 public final class HttpClientUtil {
     private final static Integer OK = 200;
     private final static Integer LIMIT_REQUEST = 429;
+    /**
+     * 全局连接池对象
+     */
+    private static final PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+
+    static {
+        // 设置最大连接数
+        connectionManager.setMaxTotal(200);
+        // 设置每个连接的路由数
+        connectionManager.setDefaultMaxPerRoute(20);
+    }
+
+    /**
+     * 获取Http客户端连接对象
+     *
+     * @param timeOut 超时时间 ms
+     * @return Http客户端连接对象
+     */
+    public static CloseableHttpClient getHttpClient(Integer timeOut) {
+        RequestConfig requestConfig = RequestConfig.custom()
+                // 获取连接超时时间
+                .setConnectionRequestTimeout(timeOut)
+                // 请求超时时间
+                .setConnectTimeout(timeOut)
+                // 响应超时时间
+                .setSocketTimeout(timeOut)
+                .build();
+        HttpRequestRetryHandler retryHandler = new HttpRequestRetryHandler() {
+            @Override
+            public boolean retryRequest(IOException exception, int executionCount, HttpContext context) {
+                if (executionCount >= 3) {
+                    // 如果已经重试了3次，就放弃
+                    return false;
+                }
+                if (exception instanceof NoHttpResponseException) {
+                    // 如果服务器丢掉了连接，那么就重试
+                    return true;
+                }
+                if (exception instanceof SSLHandshakeException) {
+                    // 不要重试SSL握手异常
+                    return false;
+                }
+                if (exception instanceof InterruptedIOException) {
+                    // 超时
+                    return true;
+                }
+                if (exception instanceof UnknownHostException) {
+                    return false;
+                }
+                if (exception instanceof ConnectTimeoutException) {
+                    return false;
+                }
+                if ((exception instanceof SSLException)) {
+                    return false;
+                }
+                HttpClientContext clientContext = HttpClientContext.adapt(context);
+                HttpRequest request = clientContext.getRequest();
+                // 如果请求是幂等的，就再次尝试
+                if (!(request instanceof HttpEntityEnclosingRequest)) {
+                    return true;
+                }
+                return false;
+            }
+        };
+        return HttpClients.custom()
+                // 把请求相关的超时信息设置到连接客户端
+                .setDefaultRequestConfig(requestConfig)
+                // 把请求重试设置到连接客户端
+                .setRetryHandler(retryHandler)
+                .setConnectionManager(connectionManager)
+                .build();
+    }
 
     public static String doPostJSON(String url, String json) {
-        return doPostJSON(url, json, null);
+        return doPost(url, json, null);
+    }
+
+    public static String doPost(String url, String json, Map<String, String> headers) {
+        CloseableHttpClient httpClient = getHttpClient(10000);
+        HttpPost httpPost = new HttpPost(url);
+        CloseableHttpResponse response = null;
+        try {
+            if (headers != null && headers.size() > 0) {
+                for (Map.Entry<String, String> entry : headers.entrySet()) {
+                    httpPost.addHeader(entry.getKey(), entry.getValue());
+                }
+            }
+            if (!StringUtils.isEmpty(json)) {
+                StringEntity entity = new StringEntity(json, "UTF-8");
+                entity.setContentEncoding("UTF-8");
+                httpPost.addHeader("Content-Type", "application/json;charset=UTF-8");
+                httpPost.setEntity(entity);
+            }
+            response = httpClient.execute(httpPost);
+            return EntityUtils.toString(response.getEntity());
+        } catch (Exception e) {
+            try {
+                httpPost.abort();
+            } catch (Exception exception) {
+                throw new RuntimeException(exception);
+            }
+            throw new RuntimeException(e);
+        } finally {
+            if (response != null) {
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     public static String doPostJSON(String url, String json, Map<String, String> headers) {
